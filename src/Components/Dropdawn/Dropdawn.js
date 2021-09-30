bc1a35e4efe76e38a6109abeb9040cf23f16e761
IluhaByrm81 <iluhaburm@yandex.ru>
2021-09-30 19:22:23 +0300

    api, get, done

--- src/Components/Dropdawn/Dropdawn.js
+++ src/Components/Dropdawn/Dropdawn.js
@@ -1,21 +1,37 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getData, getDataInfo } from "../../Api/api";
 import { Link } from "react-router-dom";
 import "./Dropdawn.css";
 
 function Dropdawn() {
   const [activ, setActiv] = useState(false);
-  const [select, setSelect] = useState("Выбор файла");
+  const [data, setData] = useState([]);
+  const [dataInfo, setDataInfo] = useState(null);
+  const [currentInfo, setCurrentInfo] = useState(null);
 
-  const options = [
-    "file1",
-    "file2",
-    "file4",
-    "file5",
-    "file6",
-    "file7",
-    "file8",
-  ];
+  useEffect(() => {
+    if (dataInfo) {
+      getDataInfo(dataInfo)
+        .then((info) => {
+          setCurrentInfo(info);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      getData(10, 0)
+        .then((data) => {
+          setData(data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [dataInfo]);
+
+  const clickHandler = (name) => {
+    setDataInfo(name);
+  };
 
   return (
     <div className="dropdawn_wraper">
@@ -25,24 +41,34 @@ function Dropdawn() {
           <button className="btn_one">Назад</button>
         </Link>
         <div className="dropdawn_btn" onClick={() => setActiv(!activ)}>
-          {select}
+          {"Покемоны"} : {dataInfo}
           <span>&#9660;</span>
         </div>
         {activ && (
           <div className="dropdawn_content">
-            {options.map((elem) => (
-              <div
-                className="dropdawn_item"
-                onClick={() => {
-                  setSelect(elem);
-                  setActiv(false);
-                }}
-              >
-                {elem}
+            {data.map((elem) => (
+              <div key={elem.name} className="dropdawn_item">
+                <div
+                  onClick={() => {
+                    clickHandler(elem.name);
+                    // setActiv(false);
+                  }}
+                >
+                  {elem.name.toUpperCase()}
+                </div>
               </div>
             ))}
           </div>
         )}
+      </div>
+      <div className="dropdawn">
+        {currentInfo ? (
+          <div className="dropdawn_content">
+            <div className="dropdawn_item">Имя : {currentInfo.name}</div>
+            <div className="dropdawn_item">Рост : {currentInfo.height} см</div>
+            <div className="dropdawn_item">Вес : {currentInfo.weight} кг</div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
